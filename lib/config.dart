2cbb07808660bb55549f727c287d3be400f69ae8
Felipe_Espinel <espinelbotina@gmail.com>
2025-05-13 10:20:35 -0500

    Versión Final II

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
-static const String apiBase = 'http://192.168.1.36:8000/api';
+static const String apiBase = 'http://192.168.174.171:8000/api';
 //http://192.168.1.36:8000/api PC
 //http://192.168.174.171:8000/api Celular
 }
